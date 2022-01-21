03dc5914bef03d5e2b49e8d866ffcf3c131b9485
ediaz13 <diazemanuel27@gmail.com>
2022-01-21 18:53:56 -0300

    136. Configure JPA and initialized data.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into currency_exchange (id, currency_from, currency_to,conversion_multiple, environment)
+values (10001, 'USD', 'ARS', 206, '');
+
+insert into currency_exchange (id, currency_from, currency_to,conversion_multiple, environment)
+values (10002, 'EUR', 'ARS', 240, '');
+
+insert into currency_exchange (id, currency_from, currency_to,conversion_multiple, environment)
+values (10003, 'AUD', 'ARS', 210, '');
