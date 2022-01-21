b1d8a1f7ee4ed79b4cb6c4b9842d24b25d7b6f4f
ediaz13 <diazemanuel27@gmail.com>
2022-01-21 19:31:36 -0300

    First commit!

---
+++ src/test/java/com/soma/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soma.microservices.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
