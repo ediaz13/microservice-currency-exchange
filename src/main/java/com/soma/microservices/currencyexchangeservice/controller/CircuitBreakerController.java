7373cff33d7c7e86e129e6e915b4cd2d4fb21b35
ediaz13 <diazemanuel27@gmail.com>
2022-01-22 21:27:40 -0300

    Retry and Fallback Methods

--- src/main/java/com/soma/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/soma/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,12 +1,31 @@
 package com.soma.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 public class CircuitBreakerController {
+
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
     @GetMapping("/sample-api")
+    //@Retry(name="sample-api", fallbackMethod = "harcodedResponse")
+
+    @CircuitBreaker(name="default", fallbackMethod = "harcodedResponse")
     public String sampleApi() {
-        return "Sample API";
+        logger.info("Sample API call receive");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy", String.class);
+        return forEntity.getBody();
+    }
+
+    public String harcodedResponse(Exception ex){
+        return "fallback-response";
     }
 }
